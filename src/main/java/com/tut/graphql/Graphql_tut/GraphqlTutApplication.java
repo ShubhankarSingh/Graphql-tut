1dd53a8e1cc8367b3a2ef24721ac1974daec614f
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-11-17 22:16:10 +0530

    Initial commit

---
+++ src/main/java/com/tut/graphql/Graphql_tut/GraphqlTutApplication.java
@@ -0,0 +1,13 @@
+package com.tut.graphql.Graphql_tut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlTutApplication.class, args);
+	}
+
+}
