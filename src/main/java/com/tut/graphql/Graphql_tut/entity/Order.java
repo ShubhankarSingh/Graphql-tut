508de0dfe67cb8a67ee6f9a6d8aa09cf95ca99ae
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-11-17 22:36:06 +0530

    Add models

---
+++ src/main/java/com/tut/graphql/Graphql_tut/entity/Order.java
@@ -0,0 +1,57 @@
+package com.tut.graphql.Graphql_tut.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int orderId;
+
+    private String orderDetail;
+
+    private String address;
+
+    private int price;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Order() {
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderDetail() {
+        return orderDetail;
+    }
+
+    public void setOrderDetail(String orderDetail) {
+        this.orderDetail = orderDetail;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
