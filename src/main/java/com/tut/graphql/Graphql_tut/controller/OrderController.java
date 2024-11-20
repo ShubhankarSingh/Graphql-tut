1412fb77f7e19db4b745027efbf39d17eea8ea87
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-11-20 12:57:25 +0530

    Code changes

--- src/main/java/com/tut/graphql/Graphql_tut/controller/OrderController.java
+++ src/main/java/com/tut/graphql/Graphql_tut/controller/OrderController.java
@@ -6,7 +6,10 @@ import com.tut.graphql.Graphql_tut.services.OrderService;
 import com.tut.graphql.Graphql_tut.services.UserService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -32,5 +35,20 @@ public class OrderController {
         order.setUser(user);
 
         return orderService.createOrder(order);
+    }
+
+    @QueryMapping
+    public List<Order> getOrders(){
+        return orderService.getAllOrders();
+    }
+
+    @QueryMapping
+    public Order getOrder(@Argument int orderId){
+        return orderService.getOrder(orderId);
+    }
+
+    @MutationMapping
+    public boolean deleteOrder(@Argument int orderId){
+        return orderService.deleteOrder(orderId);
     }
 }
